2f897e506ab811b670c064ab10c688da3deff192
Mirko kawa <82475085+Mirko-khatab@users.noreply.github.com>
2022-06-29 11:41:13 +0300

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import '../styles/index.css'
+import Layout from '../components/Layout'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+     <Layout>
+      <Component {...pageProps} />
+      </Layout>
+    </>
+  )
+}
+
+export default MyApp
